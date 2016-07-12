733ac693b8a209503b22c9a6422ed29f854a1e44
theYounger <kuyehara1@gmail.com>
2016-07-12 09:37:23 -1000

    iteratorModule steps through array and returns elements based on index argument

--- iteratorModule.js
+++ iteratorModule.js
@@ -5,10 +5,14 @@ const iteratorModule = {
   count: 0,
   init: (arr) => {
 
-    return () => {//this function will be returned and stored as var STEP. Calling this function returns the next element in array.
-      var counter = iteratorModule.count;
-      iteratorModule.count++;
-      return arr[counter];
+    return (skip /*stretch*/) => {
+      if(typeof skip !== 'number') {
+        var counter = iteratorModule.count;
+        iteratorModule.count++;
+        return arr[counter];
+      } else {
+        return arr[skip];
+      }
     };
   }
 };
